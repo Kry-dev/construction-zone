a12a36680779e328048ec17b4eafbc86882a7565
Kry-dev <yaroslavkryvda@gmail.com>
2020-06-18 16:33:45 +0300

    2nd commit

---
+++ src/blocks/modules/section-allinone/section-allinone.js
@@ -0,0 +1,13 @@
+modules.define('section-allinone', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
