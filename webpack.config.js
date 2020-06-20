366667d652263d24adc3f505df107fa91dbc0d8f
Kry-dev <yaroslavkryvda@gmail.com>
2020-06-20 23:02:46 +0300

    add some sections in draft

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    plugins: [ 
-        new webpack.ProvidePlugin({ 
-            "jQuery": "jquery", 
-            "window.jQuery": "jquery", 
-            "jquery": "jquery", 
-            "window.jquery": "jquery", 
-            "$": "jquery", 
-            "window.$": "jquery" 
-        }) 
+    plugins: [
+        new webpack.ProvidePlugin({
+            "jQuery": "jquery",
+            "window.jQuery": "jquery",
+            "jquery": "jquery",
+            "window.jquery": "jquery",
+            "$": "jquery",
+            "window.$": "jquery"
+        })
     ],
     
     entry: {
@@ -55,6 +55,7 @@ module.exports = {
 
     resolve: {
         alias: {
+            "%libs%": path.resolve(__dirname, "src/blocks/libs"),
             "%modules%": path.resolve(__dirname, "src/blocks/modules"),
             "%components%": path.resolve(__dirname, "src/blocks/components")
         }
